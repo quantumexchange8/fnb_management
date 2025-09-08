553acda1229613cafa49c7f2e6f7989dcff53b07
darren <darren.kooyz@gmail.com>
2025-09-08 11:27:38 +0800

    fix: internal error 500

--- app/Http/Middleware/InitializeTenantFromSession.php
+++ app/Http/Middleware/InitializeTenantFromSession.php
@@ -19,7 +19,8 @@ class InitializeTenantFromSession
     {
         if ($request->is('tenant/logout') || $request->is('logout')) {
             return $next($request);
-        } else {
+        } 
+        
             if (!tenancy()->initialized && $request->session()->has('tenant_id')) {
                 $tenantId = $request->session()->get('tenant_id');
                 $tenant = Tenant::find($tenantId);
@@ -28,11 +29,8 @@ class InitializeTenantFromSession
                     tenancy()->initialize($tenant);
                     // Auth::shouldUse('tenant');
                 }
-            } else {
-                Auth::shouldUse('web');
             }
             
             return $next($request);
-        }
     }
 }
